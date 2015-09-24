6a422ae5be52a0da49bc2da5ddce0a52fbdd1bfe
treehouse <mmaurier1@gmail.com>
2015-09-24 11:45:19 -0400

    Add initial R code
    modify makeCacheMatrix() and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,17 @@
 ## functions do
 
 ## Write a short comment describing this function
+x <- rbind(c(1, 1/4), c(1/4, 1)) 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- x$getsolve()
+  if(!is.null(s)){
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setsolve(m)
 }
 
 
@@ -12,4 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- NULL
+  set <- function(y) {
+    x <<- y 
+    i <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) i <<- solve
+  getsolve <- function() i
+  list(set = set, get = set,
+       setsolve = setsolve,
+       getsolve = getsolve)    
 }
